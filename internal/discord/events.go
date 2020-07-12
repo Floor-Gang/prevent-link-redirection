69485afbbb4f0386cf1f1c971f1f38e4eb8c8b20
mohsenari <mohsen@mailbox.org>
2020-07-11 21:34:15 -0400

    Added check for redirect urls or if they contain "redirect

--- internal/discord/events.go
+++ internal/discord/events.go
@@ -2,7 +2,9 @@ package discord
 
 import (
 	"log"
+	"net/http"
 	"regexp"
+	"strings"
 
 	dg "github.com/bwmarrin/discordgo"
 )
@@ -13,9 +15,21 @@ func (b *Bot) onMessage(s *dg.Session, m *dg.MessageCreate) {
 	}
 	// If the message is "ping" reply with "Pong!"
 	if m.ChannelID != b.config.ChannelID {
+		// Check if the message is a URL
+		// TODO: add support for <https://URL>
 		match, _ := regexp.MatchString("^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$", m.Content)
 		if match {
-			s.ChannelMessageSend(b.config.ChannelID, "It's a URL! "+m.Content)
+			// Send GET request to the URL
+			resp, err := http.Get(m.Content)
+			if err != nil {
+				log.Fatalf("http.Get => %v", err.Error())
+			}
+			finalURL := resp.Request.URL.String()
+
+			// Check if the response URL is the same as request url
+			if finalURL != m.Content || strings.Contains(m.Content, "redirect") {
+				s.ChannelMessageSend(b.config.ChannelID, m.Content+" is redirects to: "+finalURL)
+			}
 		}
 	}
 
